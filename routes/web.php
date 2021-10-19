717ad8813245859eb4032fba2b463e7a93285da2
Yobato <satriyobagus30077@gmail.com>
2021-10-19 09:17:33 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/login', [HomeController::class, 'login']);
+Route::get('/dashboard', [HomeController::class, 'dashboard']);
